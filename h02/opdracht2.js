05e23f4216dc75f2abbd7d66ad0859af77287c66
Daan Swart <dswart@roc-dev.com>
2020-05-22 15:48:32 +0200

    H02 klaar

---
+++ h02/opdracht2.js
@@ -0,0 +1,24 @@
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl1").style.padding = "15px 0 0 0";
+document.getElementById("bl1").style.textAlign = "center";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "15px";
+document.getElementById("bl2").style.textAlign = "center";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "40px";
+document.getElementById("bl3").style.textAlign = "right";
+
+document.getElementById("bl4").style.backgroundColor = "red";
+
+
+
+
+
+
+
+
+
+
+
