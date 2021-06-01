5d21d53fbce6d2628042f164f4c700acaaaf59de
Daswart <dswart@roc-dev.com>
2021-06-01 10:22:33 +0200

    ....

--- MemoryGame/MemoryGame.js
+++ MemoryGame/MemoryGame.js
@@ -6,14 +6,19 @@
 //let pointsAura = 0;
 let players = ["Daan", "Laura"];
 //let flippedCards = [null, null];
-
 //showWhatPlayer();
 //showScore();
+let currentPlayer = Math.floor(Math.random() * 2);
 showWhatPlayer()
 
-
-function showWhatPlayer(){
-    let currentPlayer = Math.floor(Math.random() * 2);
+//functie voor het bepalen wie aan zet is
+function showWhatPlayer() {
     document.getElementById("beurt").innerHTML = players[currentPlayer];
     console.log(players[currentPlayer])
+
+
 }
+
+//Functie voor een geschud kaartendek.
+let CatArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+
